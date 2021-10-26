ee770ea4286b0cd40c4e47cfc936f00c36609a06
novakvova <novakvova@gmail.com>
2021-10-26 16:30:10 +0300

    Add animation go

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+    
+    private float dirX;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("Start script :)");
-        if(Input.GetKeyDown("space"))
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX*moveSpeed, rb.velocity.y);
+        //Debug.Log("Horizontal "+ dirX.ToString());
+        if(Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-        
+        UpateAnimatorState();
+    }
+    void UpateAnimatorState()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
     }
 }
