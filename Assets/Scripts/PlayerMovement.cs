7d89f643973058597881198f848e093631c7a321
novakvova <novakvova@gmail.com>
2021-10-27 15:36:56 +0300

    Jump good Player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
     private Animator anim;
-    
+
+    [SerializeField] private LayerMask jumpbleGround;
+
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
 
@@ -20,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -28,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX*moveSpeed, rb.velocity.y);
         //Debug.Log("Horizontal "+ dirX.ToString());
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -60,5 +64,11 @@ public class PlayerMovement : MonoBehaviour
             state = MoventState.falling;
         }
         anim.SetInteger("state", (int)state);
+    }
+    
+    bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, 
+            Vector2.down, .1f, jumpbleGround);
     }
 }
